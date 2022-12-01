dec5dc02eb5d67127a95800613471384d51fad0d
eleoes <leo.escaroz@gmail.com>
2022-11-30 20:00:05 -0800

    setup App body

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,35 @@ import styles from './style';
 const App = () => (
 
   <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX}`}>
-    
+    {/* NAVBAR */}
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        Navbar here
+      </div>
+    </div>
+
+    {/* HERO */}
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Hero here
+      </div>
+    </div>
+
+    {/* CONTENT */}
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Statistics 
+        Business 
+        Billing 
+        CardDeal 
+        Testimonials 
+        Clients 
+        CTA 
+        Footer
+      </div>
     </div>
   </div>
 
-)
+);
 
 export default App;
