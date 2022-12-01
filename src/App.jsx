6c0e397edbd7ef5d6514b6d86fe7acef71687cb2
eleoes <leo.escaroz@gmail.com>
2022-11-30 19:42:46 -0800

    fixed css bug, postcss.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
 
-export default App
+  <div className="bg-primary w-full overflow-hidden">
+    <h1>Hello, World!</h1>
+  </div>
+
+)
+
+export default App;
