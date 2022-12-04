52f161efaa5be6716db2ea6fc2fd9374ca170724
eleoes <leo.escaroz@gmail.com>
2022-12-04 15:23:10 -0800

    fixed Testimonials display issue

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -9,7 +9,7 @@ const Billing = () => (
             {/* Gradient Start  */}
             <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient' />
                  {/* Gradient End  */}
-            <div classname='absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient' />
+            <div className='absolute z-[0] w-[50%] h-[50%] -left-1/2 bottom-0 rounded-full pink__gradient' />
         </div>
 
         <div className={layout.sectionInfo}>
